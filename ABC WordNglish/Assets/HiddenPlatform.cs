a4217f8015fda517917d902e48d961e754c52438
thayna-bezerra <arraisthayna2106@gmail.com>
2022-07-22 11:52:26 -0300

    Fourth Version
    New level made: Color mechanics to hide letters, new enemies.

---
+++ ABC WordNglish/Assets/HiddenPlatform.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HiddenPlatform : MonoBehaviour
+{
+    public SpriteRenderer platform;
+
+    void Iniciar()
+    {
+        platform = GetComponent<SpriteRenderer>();
+    }
+    
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            platform.color = new Color(0, 0, 0, 0);
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            platform.color = new Color(1, 1, 1, 1);
+        }
+    }
+}
